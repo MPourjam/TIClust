7caf1f4547b63632095ff1b4a8847bfbe25dcd2c
Mohsen Pourjam <pourjam.cs@hotmail.com>
2024-11-16 17:42:00 +0100

    get_clean_taxonomy() added

--- simple_tic.py
+++ simple_tic.py
@@ -23,6 +23,23 @@ class Taxonomy:
         'species',
         'ZOTU'
     ]
+    invalid_tax_rgx = [
+        re.compile(r"uncultured", re.IGNORECASE),
+        re.compile(r"unclassified", re.IGNORECASE),
+        re.compile(r"metagenome", re.IGNORECASE),
+        re.compile(r"unidentified", re.IGNORECASE),
+        re.compile(r"unknown", re.IGNORECASE),
+        re.compile(r"unidentified", re.IGNORECASE),
+        re.compile(r"unking", re.IGNORECASE),
+        re.compile(r"unphyl", re.IGNORECASE),
+        re.compile(r"unorder", re.IGNORECASE),
+        re.compile(r"unclass", re.IGNORECASE),
+        re.compile(r"unfamily", re.IGNORECASE),
+        re.compile(r"ungenus", re.IGNORECASE),
+        re.compile(r"unspec", re.IGNORECASE),
+        # # we don't filter incertae sedis as it could have sublevels
+        # re.compile(r"incertae", re.IGNORECASE),
+    ]
 
     def __init__(self, tax_str: str):
         tax_reg_match = self.tax_regex.search(tax_str)
@@ -30,11 +47,26 @@ class Taxonomy:
             self.tax_str = tax_reg_match.group('tax')
         else:
             self.tax_str = ''
-        self.tax_list = self.parse_taxonomy(self.delimiter)
+        self.tax_list = self.get_tax_list(self.delimiter)
 
-    def parse_taxonomy(self, delimiter: str = None) -> List[str]:
+    def get_tax_list(self, delimiter: str = None) -> List[str]:
         delimiter = delimiter if delimiter else self.delimiter
-        return self.tax_str.split(delimiter) if self.tax_str else []
+        clean_tax = self.get_clean_taxonomy(self.tax_str, delimiter)
+        return clean_tax.split(delimiter)
+
+    @classmethod
+    def get_clean_taxonomy(cls, tax_str: str, delimiter: str = None) -> str:
+        # NOTE needs testing
+        delimiter = delimiter if delimiter else cls.delimiter
+        clean_tax = []
+        for curr_tax in tax_str.split(delimiter):
+            for invalid_tax in cls.invalid_tax_rgx:
+                invalid_tax_match = invalid_tax.search(curr_tax)
+                if not invalid_tax_match and curr_tax:
+                    clean_tax.append(curr_tax)
+                    break
+
+        return delimiter.join(clean_tax)
 
     @property
     def kingdom(self):
@@ -64,18 +96,21 @@ class Taxonomy:
     def species(self):
         return self._get_level('species')
 
-    def _get_level(self, level: str):
+    def _get_level(self, level: str) -> str:
         tax_level_ind = self.level_tax_map.index(level)
         if len(self.tax_list) > tax_level_ind:
             return self.tax_list[tax_level_ind]
         return f'NA-{str(level).capitalize()}'
 
-    def get_tax_upto(self, level: str):
+    def get_tax_upto(self, level: str) -> str:
         tax_level_ind = self.level_tax_map.index(level) + 1
         sliced_tax = []
         for lev in range(tax_level_ind):
             cur_level = self.level_tax_map[lev]
-            sliced_tax.append(self._get_level(cur_level))
+            if self._get_level(cur_level):
+                sliced_tax.append(self._get_level(cur_level))
+            else:
+                raise ValueError(f"Taxonomy level {cur_level} is not available for {self.tax_str}")
         return "___".join(sliced_tax)
 
     def __repr__(self):
@@ -83,6 +118,9 @@ class Taxonomy:
 
     def __str__(self):
         return self.__repr__()
+    
+    def __hash__(self):
+        return hash(self.tax_str)
 
 
 class SeqID:
@@ -137,6 +175,9 @@ class Sequence:
 
     def is_sequence_correct(self) -> bool:
         return bool(self.seq_content_regex.match(self.sequence))
+    
+    def get_tax_seq_map(self) -> Dict[Taxonomy, 'Sequence']:
+        return {self.header.taxonomy: self}
 
     def __hash__(self):
         return hash((str(self.header), self.sequence.lower()))
@@ -162,9 +203,29 @@ class ZOTUFASTA:
                     taxonomies.append(Taxonomy(line))
         return taxonomies
 
+    def get_tax_seq_map(self) -> Dict[Taxonomy, Sequence]:
+        tax_seq_map = defaultdict(list)
+        with open(self.fasta_file, 'r', encoding='utf-8') as fasta:
+            curr_line = fasta.readline().strip()
+            seq_str = ''
+            seq_header = ''
+            while curr_line:
+                if curr_line.startswith('>'):
+                    if seq_str:
+                        seq_obj = Sequence(seq_header, seq_str)
+                        tax_seq_map.get(seq_obj.header.taxonomy).append(seq_obj)
+                        seq_str = ''
+                    seq_header = curr_line
+                else:
+                    seq_str += curr_line
+                curr_line = fasta.readline().strip()
+            if seq_str:
+                seq_obj = Sequence(seq_header, seq_str)
+                tax_seq_map[seq_obj.header.taxonomy] = seq_obj
+        return tax_seq_map
+
     def get_hash_table(self) -> Dict[int, str]:
         # NOTE suprisingly, this does not help reduce memory usage as compared to get_sequences()
-
         hash_table = {}
         with open(self.fasta_file, 'r', encoding='utf-8') as fasta:
             curr_line = fasta.readline().strip()
@@ -175,7 +236,7 @@ class ZOTUFASTA:
                 if curr_line.startswith('>'):
                     if seq_str:
                         seq_obj = Sequence(seq_header, seq_str)
-                        seq_obj_hash = header_no
+                        seq_obj_hash = hash(seq_obj)
                         hash_table[seq_obj_hash] = seq_obj.header
                         seq_str = ''
                     seq_header = curr_line
