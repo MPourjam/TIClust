8b6bbb9fe4bc9f2eccec649adcf73c9feaf883d0
Mohsen Pourjam <pourjam.cs@hotmail.com>
2025-02-12 15:31:42 +0100

    package setup updated

--- setup.py
+++ setup.py
@@ -8,22 +8,34 @@ from install_hooks import CustomInstall
 
 
 setup(
-    name='tic',
-    version='0.2',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    cmdclass={'install': CustomInstall},
-    include_package_data=True,
+    name="tic",
+    version="1.0.0",
+    description="Taxonomy Informed Clustering (TIC) is a tool for clustering bacterial sequences based on their taxonomy and hypothetically complete taxonomy levels.",
+    author="Mohsen Pourjam, Ilias Lagkouvardos",
+    author_email="pourjam.cs@hotmial.com, ilias.lagkouvardos@gmail.com",
+    # url="https://example.com",
+    project_urls={
+        "Repository": "https://github.com/yourusername/yourproject",
+    },
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    python_requires=">=3.6",
+    install_requires=[
+        "requests",
+        "toml",
+    ],
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     package_data={
-        'tic': ['bin/vsearch'],
+        "tic": ["bin/vsearch"],
     },
     entry_points={
-        'console_scripts': [
-            'simpletic=tic.cli:main',
+        "console_scripts": [
+            "simpletic=tic.cli:main",
         ],
     },
-    install_requires=[
-        'requests',
-        # 'toml',
-    ],
+    cmdclass={
+        "install": "tic.install_hooks.CustomInstall",
+    },
 )
