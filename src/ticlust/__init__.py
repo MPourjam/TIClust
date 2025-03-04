88e3aa70cc8706d46deb76ddec42048402da4bf4
Mohsen Pourjam <pourjam.cs@hotmail.com>
2025-03-04 14:35:51 +0100

    returning 0.0.0 for cases of using the modules as scripts

--- src/ticlust/__init__.py
+++ src/ticlust/__init__.py
@@ -2,8 +2,7 @@ from .ticlust import *
 try:
     from ._version import version as __version__
 except ImportError:
-    from setuptools_scm import get_version
-    __version__ = get_version(root="..", relative_to=__file__)
+    __version__ = "0.0.0"
 
 
 __all__ = ['__version__', 'ticlust']
